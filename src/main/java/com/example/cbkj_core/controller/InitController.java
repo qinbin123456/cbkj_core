4ff6051210722a516f0bc1eadf7cde958fc92371
qinbin123456 <991236860@qq.com>
2018-05-21 22:59:53 +0800

    配置系统名称到配置文件

--- src/main/java/com/example/cbkj_core/controller/InitController.java
+++ src/main/java/com/example/cbkj_core/controller/InitController.java
@@ -1,11 +1,16 @@
 package com.example.cbkj_core.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class InitController {
+
+    @Value("${system.name}")
+    private String SYSNAME;
 
     @RequestMapping("loginP")
     public String loginP(){
@@ -13,7 +18,8 @@ public class InitController {
     }
 
     @RequestMapping("toMain")
-    public String toMain(){
+    public String toMain(Model model){
+        model.addAttribute("sysName",SYSNAME);
         return "main";
     }
 
