45790063ed91fb2f2f4916c018228fad0f17d073
qinbin123456 <991236860@qq.com>
2018-05-27 22:27:50 +0800

    搭建系统

--- src/main/java/com/example/cbkj_core/annotaionUtil/LogAnnotaion.java
+++ src/main/java/com/example/cbkj_core/annotaionUtil/LogAnnotaion.java
@@ -1,4 +1,4 @@
-package com.example.cbkj_core.zAnnotaionUtil;
+package com.example.cbkj_core.annotaionUtil;
 
 
 import java.lang.annotation.*;
@@ -6,6 +6,6 @@ import java.lang.annotation.*;
 @Target({ElementType.PARAMETER, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface SysLogAnnotaion {
+public @interface LogAnnotaion {
     String description()  default "";
 }
---
+++ src/main/java/com/example/cbkj_core/annotaionUtil/TokenAnnotaion.java
@@ -0,0 +1,14 @@
+package com.example.cbkj_core.annotaionUtil;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TokenAnnotaion {
+
+    boolean toP() default false;
+    boolean submitP() default false;
+}
