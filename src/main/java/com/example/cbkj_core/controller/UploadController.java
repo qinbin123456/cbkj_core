350cb67c560559574b28b91a1b5aee7de1c1b891
admin <991236860@qq.com>
2018-06-05 18:02:14 +0800

    搭建项目

--- src/main/java/com/example/cbkj_core/controller/UploadController.java
+++ src/main/java/com/example/cbkj_core/controller/UploadController.java
@@ -34,7 +34,7 @@ public class UploadController {
     }
 
     /**
-     * 上传到阿里云OOS 简单测试
+     * 上传到阿里云OSS 简单测试
      * @param file
      * @return
      */
@@ -42,13 +42,13 @@ public class UploadController {
     @ResponseBody
     public Object fileUpload(MultipartFile file){
         String endpoint = "http://oss-cn-beijing.aliyuncs.com";
-// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
         String accessKeyId = "LTAIinqNaC5ZG4bF";
         String accessKeySecret = "NnfFpIO9sHqBuWJUmp8qbesyDAPXpN";
         String bucketName = "bucket-v1";
-// 创建OSSClient实例。
+        // 创建OSSClient实例。
         OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
-// 上传文件流。
+        // 上传文件流。
         InputStream inputStream = null;
         try {
             inputStream = file.getInputStream();
