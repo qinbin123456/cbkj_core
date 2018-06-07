f3ad13a881b92e7b094dbdb08ebc5465e8401e7f
qinbin123456 <991236860@qq.com>
2018-06-07 19:19:10 +0800

    框架搭建

--- src/main/java/com/example/cbkj_core/CbkjCoreApplication.java
+++ src/main/java/com/example/cbkj_core/CbkjCoreApplication.java
@@ -3,18 +3,10 @@ package com.example.cbkj_core;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-
 @MapperScan("com.example.cbkj_core.mapper")
-public class CbkjCoreApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(CbkjCoreApplication.class);
-    }
+public class CbkjCoreApplication {
 
 
     public static void main(String[] args) {
